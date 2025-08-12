6dcf8e1b53d263e3a83e991ed10698e8c000ca50
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2025-08-12 19:31:39 -0300

    change coordinates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public class Manager : MonoBehaviour
 {
     public GameObject perigoPrefab;
-    private const int MAX_X_POSITION = 18;
+    
+	private const int MAX_X_SPAWN_POSITION = 18;
+	private const int PERIGO_SPAWN_HEIGHT = 30;
+	private const int PLATFORM_Z_POSITION = 20;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,8 +17,8 @@ public class Manager : MonoBehaviour
 
     private IEnumerator SpawnPerigo()
     {
-        var x = UnityEngine.Random.Range(-MAX_X_POSITION, MAX_X_POSITION);
-        Instantiate(perigoPrefab, new Vector3(x, 20, 20), Quaternion.identity);
+        var spawnXPosition = UnityEngine.Random.Range(-MAX_X_SPAWN_POSITION, MAX_X_SPAWN_POSITION);
+        Instantiate(perigoPrefab, new Vector3(spawnXPosition, PERIGO_SPAWN_HEIGHT, PLATFORM_Z_POSITION), Quaternion.identity);
         yield return new WaitForSeconds(1);
         yield return SpawnPerigo();
     }
